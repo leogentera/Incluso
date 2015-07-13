bf1845c42df1de6f4f624080c4d5412f56d344bf
Guillermo <guillermo.ulloa@definityfirst.com>
2015-07-13 11:09:16 -0500

    UI Updates -  Profile view Added
    Included a controller to render it

--- MobileApplication/Scripts/app/shared/controllers/navigation.js
+++ MobileApplication/Scripts/app/shared/controllers/navigation.js
@@ -1,7 +1,7 @@
 /* JS for navigation */
 
 
-var app = angular.module('MainNavigation', ['ui.bootstrap']);
+var app = angular.module('inlcuso.shared.mainNavigation', ['ui.bootstrap']);
 
 app.controller('navController', function($scope){
 	$scope.isNavCollapsed = true;
