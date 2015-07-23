b9b7aefba5cede490500019f721f2eb62badcdf1
Bernardo Garza Garc�a <bernardo.garza@sieena.com>
2015-07-23 10:24:13 -0500

    Profile integration

--- MobileApplication/Scripts/app/models/Profile.js
+++ MobileApplication/Scripts/app/models/Profile.js
@@ -12,58 +12,93 @@
             var model = this;
             model.set(
                 {
-                    id: 2,
-                    firstname: "Fernando",
-                    fullname: "Gutierrez",
-                    username: "fernando.gutierrez",
-                    lastname: "Gtz",
-                    country: "mx",
-                    email: "bernardo.garza@definityfirst.com",
-                    studies: [{
-                        school: "Secundaria 50",
-                        levelOfStudies: "terminada"
+                    "country": null,
+                    "email": "humberto_c49@hotmail.com",
+                    "firstname": "Admin",
+                    "fullname": "Admin User",
+                    "id": 2,
+                    "lastname": "User",
+                    "profileimageurl": "http:\/\/localhost\/moodle\/pluginfile.php\/5\/user\/icon\/f1",
+                    "profileimageurlsmall": "http:\/\/localhost\/moodle\/pluginfile.php\/5\/user\/icon\/f2",
+                    "username": "admin",
+                    "studies": [{
+                        "school": "Sec. 50",
+                        "levelOfStudies": "Secundaria"
                     }],
-                    address: {
-                        street: "Alamo",
-                        num_ext: "2922",
-                        num_int: "",
-                        colony: "Bosques del contry",
-                        city: "Monterrey",
-                        town: "Monterrey",
-                        state: "Nuevo Leon",
-                        country: "mx",
-                        postalCode: "67176"
+                    "address": {
+                        "street": "Alamo",
+                        "num_ext": "2922",
+                        "num_int": "",
+                        "colony": "Bosques del contry",
+                        "city": "Monterrey",
+                        "town": "Monterrey",
+                        "state": "Nuevo Leon",
+                        "country": null,
+                        "postalCode": "67176"
                     },
-                    phones: [
-                        "02322",
-                        "3433"
-                    ],
-                    socialNetworks: [{
-                        socialNetwork: "Facebook",
-                        socialNetworkId: "casgar49"
+                    "phones": ["12345",
+                    "654987",
+                    "4113"],
+                    "socialNetworks": [{
+                        "socialNetwork": "Twitter",
+                        "socialNetworkId": "casgar49"
+                    },
+                    {
+                        "socialNetwork": "Facebook",
+                        "socialNetworkId": "casgar49"
                     }],
-                    familiaCompartamos: [{
-                        idClient: "4113",
-                        relativeName: "Papa de Humberto",
-                        relationship: "Padre"
+                    "familiaCompartamos": [{
+                        "idClient": "4113",
+                        "relativeName": "padre",
+                        "relationship": "papa"
                     }],
-                    stage: "",
-                    stars: 0,
-                    attributesAndQualities: ["Divertido", "Original"],
-                    strengths: [],
-                    recomendedBachelorDegrees: [],
-                    likesAndPreferences: ["Face", "Dibujar"],
-                    dreamsToBe: ["Ser Rico", "Poderoso"],
-                    dreamsToHave: ["Esto", "Aquello"],
-                    dreamsToDo: ["Esto", "Aquello"],
-                    badgesEarned: [],
-                    showMyInformation: true,
-                    showAttributesAndQualities: false,
-                    showLikesAndPreferences: false,
-                    showBadgesEarned: false,
-                    showStrengths: false,
-                    showRecomendedBachelorDegrees: false,
-                    showMyDreams: false
+                    "rank": 2,
+                    "stars": "20",
+                    "attributesAndQualities": ["Educado",
+                    "Atento",
+                    "Inteligente"],
+                    "strengths": [],
+                    "recomendedBachelorDegrees": [],
+                    "likesAndPreferences": ["Escuchar musica"],
+                    "badgesEarned": [],
+                    "badgesToEarn": [{
+                        "id": "1",
+                        "name": "Testing Badge",
+                        "description": "A badge for testing!",
+                        "earned_times": "1",
+                        "points": "100",
+                        "dateIssued": null
+                    },
+                    {
+                        "id": "2",
+                        "name": "Second Badge",
+                        "description": "Adventure time Badge",
+                        "earned_times": "1",
+                        "points": "101",
+                        "dateIssued": null
+                    },
+                    {
+                        "id": "3",
+                        "name": "Kemosion",
+                        "description": "meme badge",
+                        "earned_times": null,
+                        "points": "1000",
+                        "dateIssued": null
+                    }],
+                     "dreamsToBe": ["Ingeniero", "Desarrollador de Software"],
+                    "dreamsToHave": ["Carro", "Casa"],
+                    "dreamsToDo": ["Plantar un hijo", "Leer un arbol", "Tener un libro"],
+                    "showMyInformation": true,
+                    "showAttributesAndQualities": true,
+                    "showLikesAndPreferences": true,
+                    "showBadgesEarned": true,
+                    "showStrengths": true,
+                    "showRecomendedBachelorDegrees": true,
+                    "showMyDreams": false,
+                    "alias": "Administrador",
+                    "termsAndConditions": true,
+                    "informationUsage": false,
+                    "status": "Enabled"
                 }
             );
             model.trigger('sync', model, null, options);
