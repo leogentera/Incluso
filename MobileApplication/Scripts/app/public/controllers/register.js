60f21edf59060d1246e696f45a7a09e10400dc25
Guillermo Ulloa <guillermo.ulloa@sieena.com>
2015-08-01 19:06:28 -0500

    Added modal controller

--- MobileApplication/Scripts/app/public/controllers/register.js
+++ MobileApplication/Scripts/app/public/controllers/register.js
@@ -4,13 +4,14 @@ angular
     .controller('publicRegisterController', [
         '$q',
         '$scope',
+        '$modal',
         '$location',
         '$routeParams',
 		'$timeout',
 		'$rootScope',
 		'$http',
         '$anchorScroll',
-        function ($q, $scope, $location, $routeParams, $timeout, $rootScope, $http, $anchorScroll) {
+        function ($q, $scope, $location, $routeParams, $timeout, $rootScope, $http, $anchorScroll, $modal) {
 
             $scope.scrollToTop();
 
@@ -154,6 +155,82 @@ angular
 
             }
 
-        }]);
+            /* open terms and conditions modal */
+            $scope.openModal = function (size) {
+
+                $scope.items = ['item1', 'item2', 'item3'];
+
+                var modalInstance = $modal.open({
+                    animation: $scope.animationsEnabled,
+                    templateUrl: 'termsAndConditionsModal.html',
+                    controller: 'ModalInstanceCtrl',
+                    size: size
+                });
+
+                /*modalInstance.result.then(
+                    function (selectedItem) {
+                        $scope.selected = selectedItem;
+                    }, 
+                    function () {
+                        $log.info('Modal dismissed at: ' + new Date());
+                    }
+                );*/
+            };
+
+        }])
 
 
+        .controller('modalServiceControllerA', function ($scope, $modal, $log) {
+
+            $scope.items = ['item1', 'item2', 'item3'];
+
+            $scope.animationsEnabled = true;
+
+            $scope.open = function (size) {
+
+                var modalInstance = $modal.open({
+                    animation: $scope.animationsEnabled,
+                    templateUrl: 'termsAndConditionsModal.html',
+                    controller: 'ModalInstanceCtrl',
+                    size: size,
+                    resolve: {
+                        items: function () {
+                            return $scope.items;
+                        }
+                    }
+                });
+
+                modalInstance.result.then(
+                    function (selectedItem) {
+                        $scope.selected = selectedItem;
+                    }, 
+                    function () {
+                        $log.info('Modal dismissed at: ' + new Date());
+                    }
+                );
+            };
+
+            $scope.toggleAnimation = function () {
+                $scope.animationsEnabled = !$scope.animationsEnabled;
+            };
+
+        })
+
+        // Please note that $modalInstance represents a modal window (instance) dependency.
+        // It is not the same as the $modal service used above.
+
+        .controller('ModalInstanceCtrl', function ($scope, $modalInstance, items) {
+
+            $scope.items = items;
+            $scope.selected = {
+                item: $scope.items[0]
+            };
+
+            $scope.ok = function () {
+                $modalInstance.close($scope.selected.item);
+            };
+
+            $scope.cancel = function () {
+                $modalInstance.dismiss('cancel');
+            };
+        });
