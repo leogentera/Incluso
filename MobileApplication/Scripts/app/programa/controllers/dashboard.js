f667049eed89ad1cc49f946c1a07021b9be7a41e
Guillermo Ulloa <guillermo.ulloa@sieena.com>
2015-08-10 22:12:38 -0500

    Fixed Merge Problem with file changes.

--- MobileApplication/Scripts/app/programa/controllers/dashboard.js
+++ MobileApplication/Scripts/app/programa/controllers/dashboard.js
@@ -1,18 +1,18 @@
-﻿angular
+﻿﻿angular
     .module('incluso.programa.dashboard', [])
     .controller('programaDashboardController', [
         '$q',
         '$scope',
         '$location',
         '$routeParams',
-		'$timeout',
-		'$rootScope',
-		'$http',
+        '$timeout',
+        '$rootScope',
+        '$http',
         '$modal',
         function ($q, $scope, $location, $routeParams, $timeout, $rootScope, $http, $modal) {
 
             _httpFactory = $http;
-
+                    
             $scope.Math = window.Math;
 
             console.log('loading user');
@@ -24,32 +24,39 @@
             console.log('loading currentStage');
             $scope.currentStage = JSON.parse(moodleFactory.Services.GetCacheObject("currentStage"));
             console.log('loading stage');
-
-            try {
-                if (moodleFactory.Services.GetCacheObject("stage")) {
-                    $scope.stage = JSON.parse(moodleFactory.Services.GetCacheObject("stage"));
-                } else {
-                    $scope.stage = {};
-                }
-            }
-            catch (e) {
-                console.log(e);
-            }
-            console.log('finish loading from cache');
-            getDataAsync();
             
+            if(moodleFactory.Services.GetCacheObject("stage")){
+                $scope.stage = JSON.parse(moodleFactory.Services.GetCacheObject("stage"));                
+            }else{                
+                $scope.stage = {};
+            }
+            getDataAsync();
 
-            $scope.logout = function () {
-                localStorage.removeItem("CurrentUser");
-                $location.path('/');
+            $scope.logout = function(){
+                logout($http, $scope, $location);
             };
 
             $scope.navigateToStage = function(){
-                $location.path('/ProgramaDashboardEtapa/' + $scope.stage.stageId);
+                localStorage.setItem("firstTimeStage",0);                
+                $location.path('/ProgramaDashboardEtapa/' + $scope.stage.id);
             };
+            
+             $scope.playWelcome = function(){                 
+                 var videoAddress = "assets/media";
+                 var videoName = "480x270.mp4";
+                cordova.exec(Success, Failure, "CallToAndroid", "PlayLocalVideo", [videoAddress,videoName]);
+            };
+            
+            function Success() {
+                
+            }
+            
+            function Failure() {
+                
+            }
+            
 
             function getDataAsync() {
-                console.log('loading async user course');
                 moodleFactory.Services.GetAsyncUserCourse(_getItem("userId"), getDataAsyncCallback, errorCallback);
             }
 
@@ -58,8 +65,7 @@
 
                 moodleFactory.Services.GetAsyncCourse($scope.usercourse.courseId, function(){
                     $scope.course = JSON.parse(localStorage.getItem("course"));
-                    $scope.currentStage = getCurrentStage();
-
+                    $scope.currentStage = getCurrentStage();                
                     localStorage.setItem("currentStage", $scope.currentStage);
                 }, errorCallback);
             }
@@ -67,15 +73,21 @@
             function errorCallback(data){
                 console.log(data);
             }
+                            
 
-            function getCurrentStage(){
-                var currentStage = 1;
-
-                for(var i = 0; i < $scope.usercourse.stages.length; i++){
+            function getCurrentStage(){                
+                var currentStage = 1;                                            
+                
+                for(var i = 0; i < $scope.usercourse.stages.length; i++){                    
                     var uc = $scope.usercourse.stages[i];
-
+                    
                     localStorage.setItem("stage", JSON.stringify(uc));
                     $scope.stage = uc;
+                    var firstTimeStage = localStorage.getItem("firstTimeStage");                    
+                    if (firstTimeStage == 0) {
+                        $scope.stage.firstTime = 0;
+                    }                        
+                    
                     if(uc.stageStatus === 0){
                         break;
                     }
@@ -87,20 +99,13 @@
             }
 
             /* open terms and conditions modal */
-            $scope.openModal = function (size) {
+            /*$scope.openModal = function (size) {
                 var modalInstance = $modal.open({
                     animation: $scope.animationsEnabled,
                     templateUrl: 'tutorialModal.html',
-                    controller: 'tutorialController',
+                    controller: null,
                     size: size
                 });
-                console.log("modal open");
-            };
-            $scope.openModal();
+            };*/
 
-        }])
-        .controller('tutorialController', function ($scope, $modalInstance) {
-            $scope.cancel = function () {
-                $modalInstance.dismiss('cancel');
-            };
-        });
+        }]);
