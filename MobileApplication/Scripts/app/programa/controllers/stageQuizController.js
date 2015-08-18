7a0e7108941b84f1a22bcd70aa1800cd32988f07
gutierfe <fernando@sieena.com>
2015-08-18 11:41:06 -0500

    Exploracion Inicial

--- MobileApplication/Scripts/app/programa/controllers/stageQuizController.js
+++ MobileApplication/Scripts/app/programa/controllers/stageQuizController.js
@@ -12,6 +12,8 @@ angular
         '$modal',
         function ($q, $scope, $location, $routeParams, $timeout, $rootScope, $http, $anchorScroll, $modal) {
 
+            //debugger;            
+
             $rootScope.pageName = "Estación: Conócete"
             $rootScope.navbarBlue = true;
             $rootScope.showToolbar = true;
@@ -20,6 +22,19 @@ angular
 
             $scope.scrollToTop();
             $scope.$emit('HidePreloader'); //hide preloader
+
+            $scope.activity = null;
+
+            function getDataAsync() {
+                moodleFactory.Services.GetAsyncActivity(_getItem($routeParams.moodleId), getActivityInfoCallback);
+            }
+
+            function getActivityInfoCallback() {
+                debugger;
+                $scope.activity = JSON.parse(moodleFactory.Services.GetCacheObject("activity/" + $routeParams.moodleId));
+            }
+
+            getDataAsync();
 
             $scope.back = function () {
                 $location.path('/ProgramaDashboard');
