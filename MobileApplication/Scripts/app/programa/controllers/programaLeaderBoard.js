73a640856480b61833ff819a736bd94747d074f2
gutierfe <fernando@sieena.com>
2015-08-17 23:27:50 -0500

    Zona de Vuelos - init

---
+++ MobileApplication/Scripts/app/programa/controllers/programaLeaderBoard.js
@@ -0,0 +1,24 @@
+// http://weblogs.asp.net/dwahlin/archive/2013/09/18/building-an-angularjs-modal-service.aspx
+angular
+    .module('incluso.programa.leaderboard', [])
+    .controller('programaLeaderBoard', [
+        '$q',
+        '$scope',
+        '$location',
+        '$routeParams',
+		'$timeout',
+		'$rootScope',
+		'$http',
+        '$anchorScroll',
+        '$modal',
+        function ($q, $scope, $location, $routeParams, $timeout, $rootScope, $http, $anchorScroll, $modal) {
+
+            $scope.scrollToTop();
+
+
+
+
+            
+
+
+        }]);
