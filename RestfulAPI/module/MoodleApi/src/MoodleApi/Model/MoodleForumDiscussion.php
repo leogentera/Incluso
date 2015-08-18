38046cf37df5cabd49e4a8780abc50735ae22e76
danielahdzb <daniela.hernandez@definityfirst.com>
2015-08-18 14:10:40 -0500

    Foro Image Discussion

--- RestfulAPI/module/MoodleApi/src/MoodleApi/Model/MoodleForumDiscussion.php
+++ RestfulAPI/module/MoodleApi/src/MoodleApi/Model/MoodleForumDiscussion.php
@@ -6,12 +6,14 @@ class MoodleForumDiscussion{
 	public $id;
     public $name;
 	public $message;
+    public $image = array();
 	public $posts = array();
 
 	public function __construct($data){
         $this->id      = (!empty($data['id'])) ? $data['id'] : null;
         $this->name    = (!empty($data['name'])) ? $data['name'] : null;;
-        $this->message = (!empty($data['message'])) ? $data['message'] : null;    
+        $this->message = (!empty($data['message'])) ? $data['message'] : null; 
+        $this->image   = (!empty($data['attachments'])) ? $data['attachments'] : null;       
     }
 
     public function setId($id){
