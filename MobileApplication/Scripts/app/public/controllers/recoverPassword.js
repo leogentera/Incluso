c39ae29418c59a885cbb1cebaac4ffe07961ba54
UlisesBecerra <ulises.becerra@definityfirst.com>
2015-08-18 10:20:49 -0500

    Se elimina mensaje de formato en confirmar contraseña

--- MobileApplication/Scripts/app/public/controllers/recoverPassword.js
+++ MobileApplication/Scripts/app/public/controllers/recoverPassword.js
@@ -120,8 +120,7 @@ angular
                 console.log('fetching errors list'); //- debug
                 var errors = [];
                 var passwordPolicy = "debe ser almenos de 8 caracterres, incluir un caracter especial, una letra mayúscula, una minúscula y un número.";
-                if(!isConfirmedPasswordValid) { errors.push("la confirmación de contraseña no coincide con la contraseña."); }
-                if(!$scope.recoverPasswordForm.password.$valid){ errors.push("formato de contraseña incorrecto.  La contraseña " + passwordPolicy); }
+                if(!isConfirmedPasswordValid) { errors.push("la confirmación de contraseña no coincide con la contraseña."); }                
                 if(!$scope.recoverPasswordForm.confirmPassword.$valid){ errors.push("formato de confirmación de contraseña incorrecto. La confirmación de contraseña " + passwordPolicy); }
                 if(!$scope.recoverPasswordForm.code.$valid){ errors.push("código requerido."); }
                 $scope.recoverPasswordModel.modelState.errorMessages = errors;
