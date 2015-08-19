38846959dc29af10c673d4a7ca1ab7435d442797
UlisesBecerra <ulises.becerra@definityfirst.com>
2015-08-19 13:04:28 -0500

    Listado de notificaciones

--- MobileApplication/Scripts/app/programa/controllers/Alerts.js
+++ MobileApplication/Scripts/app/programa/controllers/Alerts.js
@@ -10,16 +10,28 @@ angular
         '$http',
         '$modal',
         function ($q, $scope, $location, $routeParams, $timeout, $rootScope, $http, $modal) {
-            $rootScope.pageName = "Notificaciones"
+            
+            $scope.notifications = [
+                { notificationDetail:'Has ganado una estrella', read:'false', notificationDate:'01 de Enero de 1999'},
+                { notificationDetail:'Has completado una act', read: 'false', notificationDate:'01 de Marzo de 1999'},
+                { notificationDetail:'Has completado una etapa', read:'true', notificationDate:'01 de Febrero de 1999'}
+            ];
+            
+            $rootScope.pageName = "Notificaciones";
             $rootScope.navbarBlue = false;
             $rootScope.showToolbar = true;
-            $rootScope.showFooter = true; 
-            $rootScope.showFooterRocks = false; 
-
+            $rootScope.showFooter = true;
+            $rootScope.showFooterRocks = false;
+                                            
+            $scope.read = function(item){
+                return item.read == read;                
+            }
+                    
             $scope.$emit('HidePreloader');
             
             $scope.back = function () {
                 $location.path('/ProgramaDashboard');
             }
-
-}]);
+            
+        }
+]);
