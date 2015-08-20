4b029e75e99045d544cf0b55c21c078ec9b893bf
UlisesBecerra <ulises.becerra@definityfirst.com>
2015-08-19 22:03:03 -0500

    Validación recuperar contraseña

--- MobileApplication/Scripts/app/public/controllers/recoverPassword.js
+++ MobileApplication/Scripts/app/public/controllers/recoverPassword.js
@@ -68,7 +68,7 @@ angular
                 console.log('fetching errors list'); //- debug
                 var errors = [];
                 if(!$scope.recoverPasswordForm.email.$valid){ errors.push("Formato de correo incorrecto."); }
-                if($scope.recoverPasswordModel.secretQuestion.length === 0){ errors.push("Pregunta secreta inválida."); }
+                if(!$scope.recoverPasswordModel.secretQuestion){ errors.push("Pregunta secreta inválida."); }
                 if(!$scope.recoverPasswordForm.secretAnswer.$valid){ errors.push("Respuesta secreta inválida."); }
                 $scope.recoverPasswordModel.modelState.errorMessages = errors;
 
