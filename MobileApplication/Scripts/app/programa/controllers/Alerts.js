a67a2e1340290450f5606061c1ea94f186c88065
UlisesBecerra <ulises.becerra@definityfirst.com>
2015-08-21 10:18:02 -0500

    Formato de fecha en notificaciones

--- MobileApplication/Scripts/app/programa/controllers/Alerts.js
+++ MobileApplication/Scripts/app/programa/controllers/Alerts.js
@@ -16,7 +16,7 @@ angular
                     notificationId: 1,
                     notificationTitle:'Has ganado una estrella',
                     read:'false',
-                    notificationDate:new Date('21/08/2015'),
+                    notificationDate: new Date('01/02/2005'),
                     notificationDetail : 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras venenatis velit magna, vitae commodo metus volutpat a. Morbi euismod mauris lectus. Praesent suscipit consequat felis, a aliquet nibh porttitor vitae. Praesent volutpat tortor ipsum, tempor lacinia justo aliquet ut. Maecenas dolor mauris, vestibulum non varius a, feugiat in elit. Nulla libero elit, gravida quis orci pulvinar, consequat rhoncus ex. Praesent a ultrices nisi. Ut vel volutpat tortor, a ultrices nisl. Nullam at faucibus velit. Nunc pharetra lacus nisi.'
                     },
                 {
