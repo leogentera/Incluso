203e4d8b26b2ed05ab27a0c5d7dc6a90b7f39311
analudg <analudg0@gmail.com>
2016-01-15 21:59:34 -0600

    reverted previous commit

--- AndroidApplication/platforms/android/src/com/definityfirst/incluso/MainActivity.java
+++ AndroidApplication/platforms/android/src/com/definityfirst/incluso/MainActivity.java
@@ -626,10 +626,11 @@ public class MainActivity extends CordovaActivity implements DownloadFileListene
                 String imagepath ="avatar_"+jsonObject.getString("userId")+".png"; //searchForAvatar(avatarFolder);
                 jsonObject.put("pathImagen", imagepath);
             }
-            /*if (global.getCallbackContext() != null){
-                    global.getCallbackContext().success(jsonObject);
+            if (global.getCallbackContext() != null){
+                loadUrl("javascript:_successGame("+ jsonObject.toString() +")");
+                    //global.getCallbackContext().success(jsonObject);
             }
-            else{*/
+            else{
                 preventToLoad=true;
                 final File file = new File(appFolder, "redirectToAndroid.html");
                 //final File file = new File(appFolder, "index.html");
@@ -658,8 +659,9 @@ public class MainActivity extends CordovaActivity implements DownloadFileListene
                 }else{
                     Toast.makeText(this, "Se perdió la conexión con el juego", Toast.LENGTH_SHORT).show();
                 }
+
                 loadUrl(uri.toString() + "?url=" + url + "&imacellphone=true");
-            //}
+            }
 
         } catch (JSONException e) {
             e.printStackTrace();
